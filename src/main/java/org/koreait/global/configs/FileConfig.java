8781ec010a76b4da37c8563959c6687deb769f36
SilenceTale <chomn55@gmail.com>
2024-11-28 09:50:19 +0900

    pokemon
    포켓몬 엔티티 진행전 저장

--- src/main/java/org/koreait/global/configs/FileConfig.java
+++ src/main/java/org/koreait/global/configs/FileConfig.java
@@ -1,4 +1,21 @@
 package org.koreait.global.configs;
 
-public class FileConfig {
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableConfigurationProperties(FileProperties.class)
+public class FileConfig implements WebMvcConfigurer {
+
+    private final FileProperties properties;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(properties.getUrl() + "**")
+                .addResourceLocations("file:///" + properties.getPath());
+    }
 }

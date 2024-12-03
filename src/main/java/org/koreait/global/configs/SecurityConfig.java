e5cddd9fcd459069f200ed302f02cd6550deb51f
SilenceTale <chomn55@gmail.com>
2024-12-03 16:54:08 +0900

    fixed pokemon (password version)
    password 수정, _header.html 수정

--- src/main/java/org/koreait/global/configs/SecurityConfig.java
+++ src/main/java/org/koreait/global/configs/SecurityConfig.java
@@ -2,6 +2,8 @@ package org.koreait.global.configs;
 
 import org.koreait.member.services.LoginFailureHandler;
 import org.koreait.member.services.LoginSuccessHandler;
+import org.koreait.member.services.MemberAccessDeniedHandler;
+import org.koreait.member.services.MemberAuthenticationExceptionHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -40,10 +42,25 @@ public class SecurityConfig {
          * authenticated() : 인증 받은 사용자만 접근
          * anonymous() : 인증 받지 않은 사용자만 접근
          * permitAll() : 모든 사용자가 접근 가능
+         * hasAuthority("권한 명칭") : 하나의 권한을 체크
+         * hasAnyAuthority("권한1", "권한2", ...) : 나열된 권한 중 하나라도 충족하면 접근 가능
+         * ROLE
+         * ROLE_명칭
+         * hasRole("명칭")
+         * hasAnyRole(...)
          */
         http.authorizeHttpRequests(c -> {
             c.requestMatchers("/mypage/**").authenticated()
+                    .requestMatchers("/member/login", "/member/join", "member/agree").anonymous() // 미인증 회원만 접근 가능
+                    .requestMatchers("/admin/**").hasAnyAuthority("MANAGER", "ADMIN") // 관리자 페이지는 MANAGER,와 ADMIN 권한
+                    .anyRequest().permitAll(); // 나머지 페이지는 모두 접근 가능
         });
+
+        http.exceptionHandling(c -> {
+            c.authenticationEntryPoint(new MemberAuthenticationExceptionHandler()) // 미로그인시 인가 실패
+                    .accessDeniedHandler(new MemberAccessDeniedHandler()); // 로그인 이후 인가 실패
+        });
+
         /* 인가 설정 E */
 
         return http.build(); // 설정 무력화

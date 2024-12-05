2215a3117581fc4ab530effa1e7a448425f8f952
SilenceTale <chomn55@gmail.com>
2024-12-05 12:59:44 +0900

    1234
    1234

--- src/main/java/org/koreait/file/controllers/ApiFileController.java
+++ src/main/java/org/koreait/file/controllers/ApiFileController.java
@@ -1,26 +1,19 @@
 package org.koreait.file.controllers;
 
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
 import org.koreait.global.exceptions.BadRequestException;
-import org.koreait.global.libs.Utils;
-import org.koreait.member.controllers.RequestLogin;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @RequestMapping("/api/file")
-@RequiredArgsConstructor
 public class ApiFileController {
-
-    private final Utils utils;
-
     @GetMapping
-    public void test(@Valid RequestLogin form, Errors errors) {
-        if (errors.hasErrors()) {
-            throw new BadRequestException(utils.getErrorMessages(errors));
+    public void test() {
+        boolean result = true;
+        if (result) {
+            throw new BadRequestException("그냥 테스트...");
         }
     }
 }

5a1404bf35489a68465acaf07ef55403ba7f88bb
SilenceTale <chomn55@gmail.com>
2024-12-05 15:15:16 +0900

    필요없는 어노테이션 삭제 후 수정
    1

---
+++ src/main/java/org/koreait/file/repositories/FileInfoRepository.java
@@ -0,0 +1,11 @@
+package org.koreait.file.repositories;
+
+import org.koreait.file.entities.FileInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+
+import java.io.File;
+
+public interface FileInfoRepository extends JpaRepository<FileInfo, Long>, QuerydslPredicateExecutor<FileInfo> {
+
+}

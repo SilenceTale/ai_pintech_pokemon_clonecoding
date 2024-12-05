6218a88bad34fce83cf81c88a6a47395ee71b72f
SilenceTale <chomn55@gmail.com>
2024-12-05 13:22:49 +0900

    fileinfo
    @Transient를 사용해서
    URL 파일 접근할 수 있는 주소와 경로를 설정

--- src/main/java/org/koreait/file/entities/FileInfo.java
+++ src/main/java/org/koreait/file/entities/FileInfo.java
@@ -3,6 +3,7 @@ package org.koreait.file.entities;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Transient;
 import lombok.Data;
 import org.koreait.global.entities.BaseMemberEntity;
 
@@ -18,6 +19,12 @@ public class FileInfo extends BaseMemberEntity {
     private String fileName; // 업로드시 원 파일명
     private String extension; // 파일 확장자
     private String contentType; // 파일 형식, image/png     application/...
+
+    @Transient
+    private String fileUrl; // URL로 파일 접근할 수 있는 주소 - 2차 가공
+
+    @Transient
+    private String filePath; // 파일이 서버에 있는 실제 경로
 
     private boolean done; // 파일과 연관된 작업이 완료 되었는지 여부 체크
 }

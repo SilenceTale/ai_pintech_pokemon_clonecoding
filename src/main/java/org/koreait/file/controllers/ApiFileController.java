7fcb799ca04b8f3cf78b27f51135a2facdc27e90
SilenceTale <chomn55@gmail.com>
2024-12-05 16:01:47 +0900

    1111
    2222

--- src/main/java/org/koreait/file/controllers/ApiFileController.java
+++ src/main/java/org/koreait/file/controllers/ApiFileController.java
@@ -1,13 +1,21 @@
 package org.koreait.file.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.koreait.file.entities.FileInfo;
 import org.koreait.global.libs.Utils;
 import org.koreait.global.rests.JSONData;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
+@Tag(name="파일 API", description = "파일 업로드, 조회, 삭제 기능 제공합니다.")
 @RestController
 @RequestMapping("/api/file")
 @RequiredArgsConstructor
@@ -15,8 +23,11 @@ public class ApiFileController {
 
     private final Utils utils;
 
+    @Operation(summary = "파일 업로드 처리")
+    @ApiResponse(responseCode = "201", description = "파일 업로드 처리, 업로드 성공시에는 업로드 완료된 파일 목록을 반환한다. 요청시 반드시 요청 헤더에 multipart/form-data 형식으로 전송한다.")
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/upload")
-    public JSONData upload() {
+    public JSONData upload(@RequestPart MultipartFile[] files, @Valid RequestUpload form, Errors errors) {
 
         return null;
     }
@@ -29,7 +40,7 @@ public class ApiFileController {
 
     // 파일 단일 조회
     @GetMapping("/info/{seq}")
-    public JSONData info(Long seq) {
+    public JSONData info(@PathVariable("seq") Long seq) {
 
         return null;
     }

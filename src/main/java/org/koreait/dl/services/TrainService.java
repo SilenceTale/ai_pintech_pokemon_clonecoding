227d9bc7c745bd8a3b6ed99e213a4e353248e325
SilenceTale <chomn55@gmail.com>
2024-12-10 10:21:36 +0900

    fixed
    fixed

--- src/main/java/org/koreait/dl/services/TrainService.java
+++ src/main/java/org/koreait/dl/services/TrainService.java
@@ -1,5 +1,18 @@
 package org.koreait.dl.services;
 
+import org.springframework.stereotype.Service;
 
+@Service
 public class TrainService {
+
+    public void process() {
+        try {
+            ProcessBuilder builder = new ProcessBuilder("C:\\Users\\admin\\AppData\\Local\\Programs\\Python\\Python39\\python.exe", "python", "C:/recommend/train.py", "http://localhost:3000/api/api/dl/data");
+            Process process = builder.start();
+            int exitCode = process.waitFor();
+
+        } catch (Exception e) {
+
+        }
+    }
 }

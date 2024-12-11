bfba0227024f66c1e0bd42438dbffb3e8938b07d
SilenceTale <chomn55@gmail.com>
2024-12-11 11:39:38 +0900

    1414
    1423

--- src/test/java/org/koreait/dl/tests/PythonRunTest.java
+++ src/test/java/org/koreait/dl/tests/PythonRunTest.java
@@ -2,6 +2,7 @@ package org.koreait.dl.tests;
 
 import org.junit.jupiter.api.Test;
 import org.koreait.dl.services.PredictService;
+import org.koreait.dl.services.TrainService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -17,9 +18,12 @@ public class PythonRunTest {
     @Autowired
     private PredictService service;
 
+    @Autowired
+    private TrainService trainService;
+
     @Test
     void test1() throws Exception {
-        ProcessBuilder builder = new ProcessBuilder("C:\\Users\\admin\\AppData\\Local\\Programs\\Python\\Python39\\python.exe", "D:/recommend/train.py", "http://localhost:3000/api/dl/data");
+        ProcessBuilder builder = new ProcessBuilder("C:\\Users\\admin\\AppData\\Local\\Programs\\Python\\Python39\\python.exe", "C:/recommend/train.py", "http://localhost:3000/api/dl/data");
         Process process = builder.start();
         BufferedReader reader = process.inputReader();
         reader.lines().forEach(System.out::println);
@@ -42,6 +46,8 @@ public class PythonRunTest {
     @Test
     void test3() {
        // python predict.py "[[ 1.23151481,  0.88790998,  1.6140196,   0.94127238,  1.6784415,   1.38504672, -1.57161094, -0.65513703,  0.99961796 , -0.80484811]]"
+
+        //trainService.process();
 
        int[] item =  { 123151481,  88790998,  16140196,   94127238,  16784415,   138504672, -157161094, -65513703,  99961796 , -80484811};
         List<int[]> items = List.of(item);

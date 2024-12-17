d590ed85213871a5856f292c1f50647c2ae587e3
SilenceTale <chomn55@gmail.com>
2024-12-17 18:22:05 +0900

    asdf
    asdfqwerty

--- src/main/resources/static/front/js/member/join.js
+++ src/main/resources/static/front/js/member/join.js
@@ -2,7 +2,8 @@ window.addEventListener("DOMContentLoaded", function() {
 
     const sendButton = document.getElementById("send-auth-code");
     const authCodeEl = document.getElementById("auth-code");
-    
+    const verifyButton = document.getElementById("verify-auth-code");
+
     const { emailAuth } = commonLib;
 
     frmJoin.email.addEventListener("change", function() {
@@ -29,10 +30,16 @@ window.addEventListener("DOMContentLoaded", function() {
 
         emailAuth.sendCode(email, updateTimer, function() {
             // 1. 전송 버튼의 문구를 인증코드 재 전송을 변경, 이메일을 변경하지 못하게 처리
+            //    이메일을 변경하지 못하게 처리
+            //    인증코드 입력 가능하게 처리
+            //    인증하기 버튼 노출
             const { text } = sendButton.dataset;
             sendButton.innerText = text;
 
-            frmJoin.email.readonly = true;
+            frmJoin.email.setAttribute("readonly", true);
+            authCodeEl.disabled = false;
+
+            verifyButton.classList.remove("dn");
         });
     });
 
@@ -46,8 +53,12 @@ window.addEventListener("DOMContentLoaded", function() {
             const min = Math.floor(seconds / 60);
             const sec = seconds - min * 60;
             timeStr = `${('' + min).padStart(2, '0')}:${('' + sec).padStart(2, '0')}`;
-        } else { //타이머가 0이 되면 다시 이메일을 변경 가능하게 처리
-
+        } else { //타이머가 0이 되면 다시 이메일을 변경 가능하게 처리, 인증 코드 입력 불가 처리, 인증하기 버튼 감추기
+            frmJoin.email.removeAttribute("readonly");
+            authCodeEl.value = "";
+            authCodeEl.disabled = true;
+            verifyButton.classList.remove("dn");
+            verifyButton.classList.add("dn");
         }
 
         const timerEl = document.querySelector(".auth-box .timer");

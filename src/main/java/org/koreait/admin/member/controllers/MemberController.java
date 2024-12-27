1600aea1ac6c3c246a7a6f483f4fafe21b835897
SilenceTale <chomn55@gmail.com>
2024-12-27 11:31:39 +0900

    1234
    123456789

--- src/main/java/org/koreait/admin/member/controllers/MemberController.java
+++ src/main/java/org/koreait/admin/member/controllers/MemberController.java
@@ -3,9 +3,16 @@ package org.koreait.admin.member.controllers;
 import lombok.RequiredArgsConstructor;
 import org.koreait.admin.global.menu.SubMenus;
 import org.koreait.global.annotations.ApplyErrorPage;
+import org.koreait.global.libs.Utils;
+import org.koreait.global.paging.ListData;
+import org.koreait.member.entities.Member;
+import org.koreait.member.services.MemberInfoService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @ApplyErrorPage
 @RequiredArgsConstructor
@@ -13,9 +20,61 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller("adminMemberController")
 public class MemberController implements SubMenus {
 
+    private final Utils utils;
+    private final MemberInfoService memberInfoService;
+
     @ModelAttribute("menuCode")
     public String menuCode() {
         return "member";
     }
 
+    /**
+     * 회원목록
+     * @param model
+     * @return
+     */
+    @GetMapping({"", "/list"})
+    public String list(@ModelAttribute MemberSearch search, Model model) {
+        commonProcess("list", model);
+
+        ListData<Member> data = memberInfoService.getList(search);
+
+        model.addAttribute("items", data.getItems());
+        model.addAttribute("pagination", data.getPagination());
+
+        return "admin/member/list";
+    }
+
+    /**
+     * 회원목록 수정 처리
+     * @param model
+     * @return
+     */
+    @PatchMapping("/list")
+    public String listPs(@RequestParam(name="chk", required = false) List<Integer> chks, Model model) {
+
+
+        utils.showSessionMessage("수정하였습니다.");
+        model.addAttribute("script", "parent.location.reload();");
+        return "common/_execute_script";
+    }
+
+    /**
+     * 공통 처리 부분
+     *
+     * @param mode
+     * @param model
+     */
+    private void commonProcess(String mode, Model model) {
+        mode = StringUtils.hasText(mode) ? mode : "list"; // mode 가 없을경우 리스트로 생성
+        String pageTitle = "";
+        if (mode.equals("list")) {
+            pageTitle = "회원 목록";
+        }
+
+        pageTitle += " - 회원 관리";
+
+        model.addAttribute("pageTitle", pageTitle);
+        model.addAttribute("subMenuCode", mode);
+    }
 }
--- src/main/java/org/koreait/member/services/MemberInfoService.java
+++ src/main/java/org/koreait/member/services/MemberInfoService.java
@@ -1,12 +1,21 @@
 package org.koreait.member.services;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.DateTimePath;
+import com.querydsl.core.types.dsl.StringExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.koreait.admin.member.controllers.MemberSearch;
 import org.koreait.file.entities.FileInfo;
 import org.koreait.file.services.FileInfoService;
+import org.koreait.global.paging.ListData;
+import org.koreait.global.paging.Pagination;
 import org.koreait.member.MemberInfo;
 import org.koreait.member.constants.Authority;
 import org.koreait.member.entities.Authorities;
 import org.koreait.member.entities.Member;
+import org.koreait.member.entities.QMember;
 import org.koreait.member.repositories.MemberRepository;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -14,7 +23,11 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
 @Lazy
@@ -24,6 +37,8 @@ public class MemberInfoService implements UserDetailsService {
 
     private final MemberRepository memberRepository;
     private final FileInfoService fileInfoService;
+    private final JPAQueryFactory queryFactory;
+    private final HttpServletRequest request;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -50,6 +65,103 @@ public class MemberInfoService implements UserDetailsService {
                 .member(member)
                 .authorities(authorities)
                 .build();
+    }
+
+    /**
+     * 회원 목록
+     *
+     * @param search
+     * @return
+     */
+    public ListData<Member> getList(MemberSearch search) {
+        int page = Math.max(search.getPage(), 1); // 1보다 작을경우 1페이지를 나오도록
+        int limit = search.getLimit();
+        limit = limit < 1 ? 20 : limit; // 0이 나오지 않게 판별식으로 고정
+        int offset = (page - 1) * limit;
+        QMember member = QMember.member;
+
+        /* 검색 처리 S */
+        BooleanBuilder andBuilder = new BooleanBuilder();
+        // 키워드 검색 처리 S
+        String sopt = search.getSopt(); // 검색 옵션
+        String skey = search.getSkey(); // 검색 키워드
+        sopt = StringUtils.hasText(sopt) ? sopt : "ALL";
+        /**
+         * sopt - ALL : 통합 검색 - 이메일 + 회원명 + 닉네임
+         *        NAME : 회원명 + 이메일
+         *        Email : 이메일
+         */
+        if (StringUtils.hasText(skey)) {
+            skey = skey.trim();
+            StringExpression condition;
+            if (sopt.equals("EMAIL")) {
+                condition = member.email;
+            } else if (sopt.equals("NAME")) {
+                condition = member.name.concat(member.nickName);
+            } else { // 통합 검색
+                condition = member.email.concat(member.name).concat(member.nickName);
+            }
+
+            andBuilder.and(condition.contains(skey));
+        }
+        //키워드 검색 E
+
+        // 이메일 검색
+        List<String> emails = search.getEmail();
+        if (emails != null && !emails.isEmpty()) {
+            andBuilder.and(member.email.in(emails));
+        }
+
+        // 권한 검색 S
+        List<Authority> authorities = search.getAuthority();
+        if (authorities != null && !authorities.isEmpty()) {
+
+            //List<Authorities> _authorities = authorities.stream()
+            //                .map(a -> )
+
+           // andBuilder.and(member.authorities.)
+        }
+        // 권한 검색 E
+
+        // 날짜 검색 S
+        String dateType = search.getDateType();
+        dateType = StringUtils.hasText(dateType) ? dateType : "createdAt"; // 가입일 기준
+        LocalDate sDate = search.getSDate();
+        LocalDate eDate = search.getEDate();
+
+        DateTimePath<LocalDateTime> condition;
+        if (dateType.equals("deletedAt")) condition = member.deletedAt; // 탈퇴일 기준
+        else if (dateType.equals("credentialChangedAt")) condition = member.credentialChangedAt; // 비밀번호 변경일 기준
+        else condition = member.createdAt; // 가입일 기준
+
+        if (sDate != null) {
+            andBuilder.and(condition.after(sDate.atStartOfDay())); //atStartOfday는 자정으로 시간을 셋업
+        }
+
+        if (eDate != null) {
+            andBuilder.and(condition.before(eDate.atTime(LocalTime.of(23,59, 59))));
+        }
+
+        // 날짜 검색 E
+
+        /* 검색 처리 E */
+
+        // 검색조건 만들기
+        List<Member> items = queryFactory.selectFrom(member)
+                .leftJoin(member.authorities)
+                .fetchJoin() // fetchJoin 을 해야 leftJoin 전체를 가져옴!
+                .where(andBuilder) // andBuilder 에서 정보를 가져옴!
+                .orderBy(member.createdAt.desc()) // 최신순으로 내림순
+                .offset(offset) // 시작 위치 설정
+                .limit(limit)
+                .fetch();
+
+        long total = memberRepository.count(andBuilder); // 총 회원 수
+        total = 100000L; // 임시로 넣음!
+        Pagination pagination = new Pagination(page, (int)total, 10, limit, request);
+
+        return new ListData<>(items, pagination); // 2차 가공하지않고 ListData로 정보 기입
+//        return null; // null 값 넣어서 임시로 오류안나게 제어
     }
 
     /**

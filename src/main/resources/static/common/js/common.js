55d7d33dccab7dfacc1df6db91159e4932e4b2c4
SilenceTale <chomn55@gmail.com>
2024-12-31 16:45:59 +0900

    asfasf
    afasfasdfasdf

--- src/main/resources/static/common/js/common.js
+++ src/main/resources/static/common/js/common.js
@@ -158,6 +158,39 @@ commonLib.popupClose = function() {
     layerEls.forEach(el => el.parentElement.removeChild(el));
 };
 
+/**
+* 위지윅 에디터 로드
+*
+*/
+commonLib.loadEditor = function(id, height = 350) {
+    if (typeof ClassicEditor === 'undefined' || !id) {
+        return;
+    }
+
+
+    return new Promise((resolve, reject) => {
+        (async() => {
+            try {
+                const editor = await ClassicEditor.create(document.getElementById(id));
+                resolve(editor);
+                editor.editing.view.change((writer) => {
+                    writer.setStyle(
+                        "height",
+                        `${height}px`,
+                        editor.editing.view.document.getRoot()
+                        );
+                });
+
+            } catch (err) {
+                console.log(err);
+
+                reject(err);
+            }
+        })();
+    });
+
+};
+
 window.addEventListener("DOMContentLoaded", function() {
     // 체크박스 전체 토글 기능 S
     const checkAlls = document.getElementsByClassName("check-all");
--- src/main/resources/static/front/js/message/send.js
+++ src/main/resources/static/front/js/message/send.js
@@ -1,1 +1,17 @@
 // 파일을 올릴 경우 후속처리를 위한 JS
+window.addEventListener("DOMContentLoaded", function() {
+    const { loadEditor } = commonLib;
+
+    loadEditor("content", 350)
+        .then((editor) => {
+            window.editor = editor; // 전역 변수로 등록, then 구간 외부에서도 접근 가능하게 처리
+        });
+});
+
+/**
+* 파일 업로드 완료 후 성공 후속 처리
+*
+*/
+function callbackFileUpload(files) {
+    console.log(files);
+}

fbb391f50f2f667864b55fef6cac6b4eb0160b71
SilenceTale <chomn55@gmail.com>
2025-01-03 13:11:20 +0900

    tqtqtqtqtq
    tqtq

--- src/main/java/org/koreait/message/services/MessageInfoService.java
+++ src/main/java/org/koreait/message/services/MessageInfoService.java
@@ -14,13 +14,37 @@ import org.springframework.stereotype.Service;
 public class MessageInfoService {
     private final MessageRepository messageRepository;
 
+    /**
+     * 쪽지 하나 조회
+     *
+     * @param seq
+     * @return
+     *
+     */
     public Message get(Long seq) {
+
+        Message item = messageRepository.findById(seq).orElseThrow();
 
         return null;
     }
 
+    /**
+     * 쪽지 목록 조회
+     *
+     * @param search
+     * @return
+     */
     public ListData<Message> getList(MessageSearch search) {
 
         return null;
+    }
+
+    /**
+     * 추가 정보 처리
+     *
+     * @param item
+     */
+    private void addInfo(Message item) {
+
     }
 }

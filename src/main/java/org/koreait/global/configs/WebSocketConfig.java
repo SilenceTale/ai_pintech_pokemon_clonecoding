c3306cf2d612d9a185c0aaa549b3bb4cd7c34833
SilenceTale <chomn55@gmail.com>
2025-01-06 15:34:45 +0900

    WebSocket설정 클래스 생성
    웹 소켓 설정할 클래스를 기초 토대를 생성했습니다

---
+++ src/main/java/org/koreait/global/configs/WebSocketConfig.java
@@ -0,0 +1,14 @@
+package org.koreait.global.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+public class WebSocketConfig implements WebSocketConfigurer { // 웹 소켓 설정
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+
+    }
+}

5827e1bbc8139868b30e4f1c8f3b94c88907b7eb
SilenceTale <chomn55@gmail.com>
2025-01-07 14:43:59 +0900

    .
    .

--- src/main/java/org/koreait/admin/board/controllers/RequestBoard.java
+++ src/main/java/org/koreait/admin/board/controllers/RequestBoard.java
@@ -19,8 +19,15 @@ public class RequestBoard {
     private int pageRangesMobile; // 모바일 기준 Front 뷰일때 노출되는 페이지 링크 갯수
     private boolean useEditor; // 에디터 사용 여부 체크
     private boolean useEditorImage; // 에디터 첨부 이미지 노출 사용 여부 체크
-    private boolean useAttachFile; // 다운로드된 첨부 파일 사용 여부
+    private boolean useAttachFile; // 다운로드된 첨부 파일 사용 여부 체크
+    private boolean useComment; // 댓글 사용 여부 체크
+    private String skin; // 게시판 스킨
 
+    /**
+     * ALL - 비회원 + 회원 + 관리자
+     * USER - 회원 + 관리자
+     * ADMIN - 관리자
+     */
     private Authority listAuthority; // 목록 접근 권한 설정
     private Authority viewAuthority; // 글 보기 접근 권한 설정
     private Authority writeAuthority; // 글 쓰기, 수정, 삭제 권한 설정

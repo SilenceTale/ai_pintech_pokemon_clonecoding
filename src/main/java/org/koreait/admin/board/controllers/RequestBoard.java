3c6c2a0267f5e44433897bc747a07f9ad8d9d34a
SilenceTale <chomn55@gmail.com>
2025-01-07 14:26:21 +0900

    1423
    14234

---
+++ src/main/java/org/koreait/admin/board/controllers/RequestBoard.java
@@ -0,0 +1,28 @@
+package org.koreait.admin.board.controllers;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import org.koreait.member.constants.Authority;
+
+@Data
+public class RequestBoard {
+    @NotBlank
+    private String bid; // 게시판 id
+
+    @NotBlank
+    private String name; // 게시판 명
+
+    private boolean open; // 공개 글인지 비공개 글인지 설정
+    private String category; // 줄 개행 문자로 여러 분류를 등록한다
+    private int rowsPerPage; // 1페이지당 게시글 갯수
+    private int pageRanges; // pc 기준 Front 뷰일때 노출되는 페이지 링크 갯수
+    private int pageRangesMobile; // 모바일 기준 Front 뷰일때 노출되는 페이지 링크 갯수
+    private boolean useEditor; // 에디터 사용 여부 체크
+    private boolean useEditorImage; // 에디터 첨부 이미지 노출 사용 여부 체크
+    private boolean useAttachFile; // 다운로드된 첨부 파일 사용 여부
+
+    private Authority listAuthority; // 목록 접근 권한 설정
+    private Authority viewAuthority; // 글 보기 접근 권한 설정
+    private Authority writeAuthority; // 글 쓰기, 수정, 삭제 권한 설정
+    private Authority commentAuthority; // 댓글 작성, 삭제 권한 설정
+}

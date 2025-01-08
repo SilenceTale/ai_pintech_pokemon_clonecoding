cc5f70aab6f4ed640b9725a5842e986fe3e3e961
SilenceTale <chomn55@gmail.com>
2025-01-08 14:44:08 +0900

    1
    1

--- src/main/java/org/koreait/board/validators/BoardValidator.java
+++ src/main/java/org/koreait/board/validators/BoardValidator.java
@@ -28,12 +28,16 @@ public class BoardValidator implements Validator {
         }
 
         RequestBoard form = (RequestBoard) target; // RequestBoard를 타겟으로 지정
+        // 비회원 비밀번호 없음
         if (!memberUtil.isLogin()) { // 로그인이 아닐때
             ValidationUtils.rejectIfEmptyOrWhitespace(errors, "guestPw", "NotBlank"); // 필드를 검증하여 비어있을경우 NotBlank 에러코드 반환
         }
 
+        // 수정일 때 seq 필수 여부
         String mode = form.getMode();
         Long seq = form.getSeq();
-        if (mode != null && mode.equals())
+        if (mode != null && mode.equals("edit") && (seq == null || seq < 1L)) {
+            errors.rejectValue("seq", "NotNull");
+        }
     }
 }

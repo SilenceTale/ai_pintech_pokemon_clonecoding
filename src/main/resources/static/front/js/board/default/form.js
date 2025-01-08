aa17e42e1e41f431063bfaee32c0a0d272c8689a
SilenceTale <chomn55@gmail.com>
2025-01-08 15:28:15 +0900

    .
    .

---
+++ src/main/resources/static/front/js/board/default/form.js
@@ -0,0 +1,7 @@
+window.addEventListener("DOMContentLoaded", function() {
+   const { loadEditor } = commonLib;
+
+   loadEditor("content", 350)
+        .then(editor => window.editor = editor);
+
+});

29f8b4eadb48ce282023835164968b104c015b2e
SilenceTale <chomn55@gmail.com>
2025-01-08 09:55:05 +0900

    1
    1

--- src/main/java/org/koreait/pokemon/entities/VotePokemon.java
+++ src/main/java/org/koreait/pokemon/entities/VotePokemon.java
@@ -1,17 +1,17 @@
-package org.koreait.pokemon.entities;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
-public class VotePokemon {
-    @Entity
-    public class Pokemon {
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long seq;
-        private String name;
-        private boolean recommended; // 추천 여부를 나타내는 필드
-    }
-}
+//package org.koreait.pokemon.entities;
+//
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.GenerationType;
+//import jakarta.persistence.Id;
+//
+//public class VotePokemon {
+//    @Entity
+//    public class Pokemon {
+//        @Id
+//        @GeneratedValue(strategy = GenerationType.IDENTITY)
+//        private Long seq;
+//        private String name;
+//        private boolean recommended;
+//    }
+//}

c7d3db94c59168379209a34219c941520b95cbcc
SilenceTale <chomn55@gmail.com>
2025-01-09 16:21:49 +0900

    1

--- src/main/java/org/koreait/board/controllers/BoardController.java
+++ src/main/java/org/koreait/board/controllers/BoardController.java
@@ -7,6 +7,7 @@ import org.koreait.board.entities.Board;
 import org.koreait.board.entities.BoardData;
 import org.koreait.board.services.BoardInfoService;
 import org.koreait.board.services.BoardUpdateService;
+import org.koreait.board.services.BoardViewUpdateService;
 import org.koreait.board.services.configs.BoardConfigInfoService;
 import org.koreait.board.validators.BoardValidator;
 import org.koreait.file.constants.FileStatus;
@@ -40,6 +41,7 @@ public class BoardController {
     private final BoardValidator boardValidator;
     private final BoardUpdateService boardUpdateService;
     private final BoardInfoService boardInfoService;
+    private final BoardViewUpdateService boardViewUpdateService;
 
     /**
      * 사용자별 공통 데이터
@@ -80,6 +82,10 @@ public class BoardController {
     public String view(@PathVariable("seq") Long seq, Model model) {
         commonProcess(seq, "view", model);
 
+        long viewCount = boardViewUpdateService.process(seq); // 조회수 업데이트
+        BoardData data = (BoardData)model.getAttribute("boardData");
+        data.setViewCount(viewCount);
+
         return utils.tpl("board/view");
     }
 
@@ -112,7 +118,7 @@ public class BoardController {
      * @return
      */
     @GetMapping("/edit/{seq}")
-    public String edit(@PathVariable("seq") Long seq, Model model, @SessionAttribute("CommonValue") CommonValue commonValue) {
+    public String edit(@PathVariable("seq") Long seq, Model model, @SessionAttribute("commonValue") CommonValue commonValue) {
         commonProcess(seq, "edit", model);
 
         RequestBoard form = boardInfoService.getForm(commonValue.getData());

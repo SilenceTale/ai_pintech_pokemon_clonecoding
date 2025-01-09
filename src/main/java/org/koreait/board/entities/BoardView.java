7972d20bc45a1e2a0b0fd0fdb5201aeb8aab5993
SilenceTale <chomn55@gmail.com>
2025-01-09 15:40:07 +0900

    1
    1

--- src/main/java/org/koreait/board/entities/BoardView.java
+++ src/main/java/org/koreait/board/entities/BoardView.java
@@ -1,10 +1,15 @@
 package org.koreait.board.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
 @Entity
 public class BoardView {
+    @Id
     private Long seq; // 게시글 번호
+
+    @Id
+    private int hash; // 회원 번호 또는 IP + User-Agent로 조합해서 생성
 }

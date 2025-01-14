224eb5db697a3d42d0ffa18bd8604bfbe87e0a7c
SilenceTale <chomn55@gmail.com>
2025-01-14 17:43:33 +0900

    .
    .

--- src/main/java/org/koreait/board/services/configs/BoardConfigUpdateService.java
+++ src/main/java/org/koreait/board/services/configs/BoardConfigUpdateService.java
@@ -48,6 +48,8 @@ public class BoardConfigUpdateService {
         String locationAfterWriting = form.getLocationAfterWriting();
         board.setLocationAfterWriting(StringUtils.hasText(locationAfterWriting) ? locationAfterWriting : "list");
 
+        board.setListUnderView();
+
         boardRepository.saveAndFlush(board);
     }
 

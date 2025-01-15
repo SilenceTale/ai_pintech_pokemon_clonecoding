0220422c625639eaf580be20fce14db585107370
SilenceTale <chomn55@gmail.com>
2025-01-15 14:14:35 +0900

    .
    .

--- src/main/java/org/koreait/board/entities/CommentData.java
+++ src/main/java/org/koreait/board/entities/CommentData.java
@@ -5,31 +5,37 @@ import lombok.Data;
 import org.koreait.global.entities.BaseEntity;
 import org.koreait.member.entities.Member;
 
+import java.io.Serializable;
+
 @Data
 @Entity
-public class CommentData extends BaseEntity {
+@Table(indexes = @Index(name = "idx_comment_data_created_at", columnList = "createdAt ASC"))
+public class CommentData extends BaseEntity implements Serializable {
     @Id @GeneratedValue
-    private Long seq; // 수정할때만 필수로 검증할 수 있게 넣음
+    private Long seq;
 
     @ManyToOne(fetch = FetchType.LAZY)
     private Member member;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    private BoardData Data; // 한개의 데이터에 여러 데이터가 들어오기에 ManyToOne 사용
+    private BoardData data;
 
-    @Column(length = 40, nullable = false)
-    private String commenter; // 댓글 작성자
+    @Column(length=40, nullable = false)
+    private String commenter;
 
-    @Column(length = 65)
-    private String guestPw; // 비회원 비밀번호
+    @Column(length=65)
+    private String guestPw;
 
     @Lob
     @Column(nullable = false)
-    private String Content; //댓글 내용
+    private String content;
 
-    @Column(length = 20)
-    private String IpAddr;
+    @Column(length=20)
+    private String ipAddr;
 
-    @Column(length = 150)
-    private String UserAgent; // 브라우저에 관한 기록
+    @Column(length=150)
+    private String userAgent;
+
+    @Transient
+    private boolean editable; // 댓글 수정, 삭제 가능 여부
 }
